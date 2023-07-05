d9bb9b570045fd15a2016041aea3327f8e09582e
jessicaraissapessoa <jessicaraissapessoa@gmail.com>
2023-07-05 20:20:17 -0300

    Variáveis var e val

---
+++ src/main/kotlin/introKotlin/variaveisVarVal.kt
@@ -0,0 +1,31 @@
+package introKotlin
+
+fun main() {
+
+    var a: String = "inicial" //var é variável mutável. Pode reatribuir o valor
+    val b: Int = 1 //val é valor imutável
+    val c = 3 //Inferência de tipo do Kotlin. O programa compreende qual o tipo
+
+    println(a) //Imprime a como "inicial"
+    println(b)
+    println(c)
+
+    //alterando a
+    a = "final"
+    println(a) //Imprime a como "final"
+
+
+
+    fun someCondition() = true
+
+    val d: Int //Pode ser inicializado posteriormente
+
+    if (someCondition()) {
+        d = 1
+    } else {
+        d = 2
+    }
+
+    println(d) //d já terá sido inicializada, podendo ser utilizada
+
+}
