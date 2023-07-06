4b438fea759fbafa1d27adaf1b00c2a06534ec68
jessicaraissapessoa <jessicaraissapessoa@gmail.com>
2023-07-06 17:25:07 -0300

    When Statement

---
+++ src/main/kotlin/estruturasDeFluxoEColecoes/whenStatement.kt
@@ -0,0 +1,25 @@
+package estruturasDeFluxoEColecoes
+
+fun main() {
+
+    //When statement
+    cases("Hello")
+    cases(1)
+    cases(0L)
+    cases(MyClass())
+    cases("hello")
+
+}
+
+//When statement
+fun cases(obj: Any) {
+    when (obj) {
+        1 -> println("One")
+        "Hello" -> println("Greeting")
+        is Long -> println("Long") // Se for do tipo Long faz...
+        !is String -> println("Not a string") // "not is" - Se não for String faz...
+        else -> println("Unknown") // Se não atender nenhum caso faz...
+    }
+}
+
+class MyClass
