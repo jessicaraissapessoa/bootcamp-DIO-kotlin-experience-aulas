5a246690113cc6ba654b4018dfa5a26c265ac058
jessicaraissapessoa <jessicaraissapessoa@gmail.com>
2023-07-06 17:49:19 -0300

    When Expression e Loops: For

---
+++ src/main/kotlin/estruturasDeFluxoEColecoes/loopsFor.kt
@@ -0,0 +1,11 @@
+package estruturasDeFluxoEColecoes
+
+fun main() {
+
+    val cakes = listOf("carrot", "cheese", "chocolate") //Lista. Função disponível de maneira implícita. listOf recebe um vararg
+
+    for (cake in cakes) { //Cada String seja atribuída à variável cake do for
+        println("Yummy, it's a $cake cake")
+    }
+
+}
---
+++ src/main/kotlin/estruturasDeFluxoEColecoes/whenExpression.kt
@@ -0,0 +1,22 @@
+package estruturasDeFluxoEColecoes
+
+fun main() {
+
+    println(whenAssign("Hello"))
+    println(whenAssign(3.4))
+    println(whenAssign(1))
+    println(whenAssign(MyClass2()))
+
+}
+
+fun whenAssign(obj: Any): Any { //função que recebe qualquer coisa e retorna qualquer coisa
+    val result = when(obj) {
+        1 -> "One"
+        "Hello" -> 1
+        is Long -> false
+        else -> 42
+    }
+    return result
+}
+
+class MyClass2
