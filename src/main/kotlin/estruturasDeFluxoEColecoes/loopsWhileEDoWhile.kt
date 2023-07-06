47553f83e4f85488188680655489592e0524bbf9
jessicaraissapessoa <jessicaraissapessoa@gmail.com>
2023-07-06 18:12:10 -0300

    Loops: While e do while

---
+++ src/main/kotlin/estruturasDeFluxoEColecoes/loopsWhileEDoWhile.kt
@@ -0,0 +1,24 @@
+package estruturasDeFluxoEColecoes
+
+fun eatACake() = println("Eat a Cake")
+fun bakeACake() = println("Bake a Cake")
+
+fun main() {
+
+    var cakesEaten = 0
+    var cakesBaked = 0
+
+    //While - Executa enquanto a condição dela for verdadeira. Começa pela condição
+    while (cakesEaten < 5) {
+        eatACake()
+        cakesEaten++
+    }
+
+
+    //Do While - Primeiro faz e daí vai para a condição
+    do { //Imprime 5 vezes
+        bakeACake()
+        cakesBaked++
+    } while (cakesBaked < cakesEaten)
+
+}
