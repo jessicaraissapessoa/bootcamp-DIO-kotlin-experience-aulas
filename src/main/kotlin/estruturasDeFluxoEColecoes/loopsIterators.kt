9e545e44d5150aae222291694f1bd0e3117eba58
jessicaraissapessoa <jessicaraissapessoa@gmail.com>
2023-07-06 18:30:36 -0300

    Loops: Iterators

---
+++ src/main/kotlin/estruturasDeFluxoEColecoes/loopsIterators.kt
@@ -0,0 +1,19 @@
+package estruturasDeFluxoEColecoes
+
+class Animal(val name: String)
+class  Zoo(val animals: List<Animal>) { //Classe Zoo que recebe uma lista imutável de Animals
+    operator fun iterator(): Iterator<Animal> {
+        return animals.iterator()
+    }
+}
+
+fun main() {
+
+    val animals = listOf(Animal("zebra"), Animal("lion"))
+    //ou val zoo = Zoo(listOf(Animal("zebra"), Animal("lion")))
+    val zoo = Zoo(animals)
+
+    for (animal in zoo) {
+        println("Watch out, it's a ${animal.name}")
+    }
+}
